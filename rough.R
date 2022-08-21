6b513a03dbb82016c717708edba82c9c0b547a1a
CourseraPai <mp_citrix@yahoo.com>
2022-08-21 18:14:44 +0530

    Initial commit

---
+++ rough.R
@@ -0,0 +1,11 @@
+library(devtools)
+find_rtools()
+has_devel()
+packageVersion("knitr")
+packageVersion("roxygen2")
+packageVersion("testthat")
+.libPaths()
+
+create_package("c:/R/Data/datamanip")
+use_git()
+
